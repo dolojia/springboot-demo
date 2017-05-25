12600753757b4f3dad57dbdbb627d1f1c69efa30
dolojia <89159107@qq.com>
2017-05-25 09:48:16 +0800

    add log4j2

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,6 +4,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -14,11 +16,19 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableAutoConfiguration
 public class DemoApplication {
 
+	Logger logger = LogManager.getLogger(DemoApplication.class);
+
 	@Autowired
 	private EnginesMapper enginesMapper;
 
 	@RequestMapping("/")
 	public String home() {
+		System.out.println("[log4j2] this is system.out");
+		logger.trace("[log4j2] this is start");
+		logger.debug("[log4j2] this is debug");
+		logger.info("[log4j2] this is info");
+		logger.warn("[log4j2] this is warn");
+		logger.error("[log4j2] this is error");
 		return "Hello World!";
 	}
 
